6a05217adb334d38794a8dcb40b3b51a46e4bfb4
Вусал Алиев <aliev_vusal_1999@mail.ru>
2020-12-27 14:15:47 +0500

    fix

--- TelegramBot/Models/Commands/MusicCommand.cs
+++ TelegramBot/Models/Commands/MusicCommand.cs
@@ -38,23 +38,6 @@ namespace TelegramBot.Models.Commands
             client.SendTextMessageAsync(chatId, allMusicsName);
 
             musics.Clear();
-
-            //music = musics.ElementAt(random.Next(0, musics.Count));
-
-            //using (var stream = System.IO.File.OpenRead("E:\\music\\" + music.Url))
-            //{
-            //    await client.SendPhotoAsync(
-            //        chatId: chatId,
-            //        photo: music.Photo                    
-            //        );
-            //    await client.SendAudioAsync(
-            //      chatId: chatId,
-            //      audio: stream,
-            //      performer: music.Name,
-            //      title: music.Performer,
-            //      duration: music.Duration
-            //    );
-            //}
         } 
     }
 }
