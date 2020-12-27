d15a4da6c699ca81be1a9c1e4a14146de7a312c0
Вусал Алиев <aliev_vusal_1999@mail.ru>
2020-12-28 00:40:32 +0500

    mini refactor

--- TelegramBot/Models/Commands/DefaultCommand.cs
+++ TelegramBot/Models/Commands/DefaultCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -18,33 +19,25 @@ namespace TelegramBot.Models.Commands
 
             string[] files = Directory.GetFiles("E:\\music");
 
-            Music music = new Music();
-
             for (int i = 0; i < files.Length; i++)
             {
                 if (files[i].Split('\\')[2].ToLower().Contains(message.Text.ToLower()))
                 {
                     var tag = TagLib.File.Create(files[i]);
-                    music.Name = tag.Name.Split('\\')[2].ToLower();
-                    music.Duration = (int)tag.Properties.Duration.TotalSeconds;
 
                     using (var stream = System.IO.File.OpenRead(files[i]))
                     {
-                        //if (!string.IsNullOrEmpty(music.Photo))
-                        //{
-                        //    await client.SendPhotoAsync(
+                        //await client.SendPhotoAsync(
                         //          chatId: chatId,
-                        //          photo: music.Photo
+                        //          photo: tag.Tag.Album
                         //          );
-                        //}
                         await client.SendAudioAsync(
                            chatId: chatId,
                            audio: stream,
-                           title: music.Name,
-                           duration: music.Duration
+                           title: tag.Name.Split('\\')[2].ToLower(),
+                           duration: (int)tag.Properties.Duration.TotalSeconds
                         );
                     }
-
                     break;
                 }
             }
